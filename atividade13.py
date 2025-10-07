6a8aaa9eb2d5b20eedf8812ced31400b0dd23ee2
Dadson097 <antoniodadsonoliveira@gmail.com>
2025-10-07 12:51:29 -0300

    Update atividade13.py

--- atividade13.py
+++ atividade13.py
@@ -1,6 +1,4 @@
 # Peça ao usuário para digitar 3 notas e armazene em uma lista. Depois, calcule a média dessas notas.
-nota1 = float(input("Digite uma nota:  "))
-nota2 = float(input("Digite outra nota: "))
-nota3 = float(input("Digite outra nota: "))
-notas = [nota1,nota2,nota3]
-print(f"As notas dos alunos foram:\n{notas} e a média foi: {(nota1+nota2+nota3)/3:.2f}")
+notas = [float(input("Digite uma nota: ")), float(input("Digite outra nota: ")), float(input("Digite outra nota: "))]
+media = (notas[0] + notas[1] + notas[2]) /3
+print(f"{media:.2f}")
